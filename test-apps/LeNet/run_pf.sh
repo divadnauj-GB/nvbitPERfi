46e9adeddf2717691d37f3edddf1dc8e5e00f8dc
divadnauj-GB <juan.guerrero@polito.it>
2022-10-31 18:34:19 +0100

    nvbitPERfi v0.0

--- test-apps/LeNet/run_pf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#export NVBITFI_HOME=/home/juancho/Documents/GitHub/Ampere_NVBit/nvbit_release/tools/Ampere_nvbitfi
-#export DARKNET_HOME=/home/juancho/Documents/GitHub/darknet_jd_v1
-
-cd $NVBITFI_HOME/pf_injector/
-
-
-#LD_PRELOAD=$NVBITFI_HOME/pf_injector/pf_injector.so $DARKNET_HOME/darknet classifier predict $DARKNET_HOME/LeNet/cfg/mnist.data $DARKNET_HOME/LeNet/cfg/mnist_lenet.cfg $DARKNET_HOME/LeNet/mnist_lenet.weights $DARKNET_HOME/LeNet/mnist_images/test/0_Five.png -t 10 > $NVBITFI_HOME/test-apps/LeNet/stdout.txt 2> $NVBITFI_HOME/test-apps/LeNet/stderr.txt cat nvbitfi-injection-log-temp.txt 
-LD_PRELOAD=$NVBITFI_HOME/pf_injector/pf_injector.so $DARKNET_HOME/darknet classifier test $DARKNET_HOME/LeNet/cfg/mnist.data $DARKNET_HOME/LeNet/cfg/mnist_lenet.cfg $DARKNET_HOME/LeNet/mnist_lenet.weights $DARKNET_HOME/LeNet/mnist_images/test/25045_One.png -t 10 > $NVBITFI_HOME/test-apps/LeNet/stdout.txt 2> $NVBITFI_HOME/test-apps/LeNet/stderr.txt cat nvbitfi-injection-log-temp.txt 
--- test-apps/LeNet/run_pf_profile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-LD_PRELOAD=/$NVBITFI_HOME/profiler/profiler.so $DARKNET_HOME/darknet classifier predict $DARKNET_HOME/LeNet/cfg/mnist.data $DARKNET_HOME/LeNet/cfg/mnist_lenet.cfg $DARKNET_HOME/LeNet/mnist_lenet.weights $DARKNET_HOME/LeNet/mnist_images/test/25045_One.png -t 10
-
-
