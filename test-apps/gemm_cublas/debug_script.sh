623e2647d97d8a8bf76b1bbbc41aad1c30c2c776
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2022-11-03 11:48:07 +0100

    Fix build error of gemm cublas example.

--- test-apps/gemm_cublas/debug_script.sh
+++ test-apps/gemm_cublas/debug_script.sh
@@ -7,11 +7,10 @@ export TOOL_VERBOSE=1
 export VERBOSE=1
 
 export NVBITFI_HOME=$CWD
-export CUDA_BASE_DIR=/usr/local/cuda-10.2
+export CUDA_BASE_DIR=/usr/local/cuda
 export PATH=$PATH:$CUDA_BASE_DIR/bin
 export LD_LIBRARY_PATH=$CUDA_BASE_DIR/lib64/:$CUDA_BASE_DIR/extras/CUPTI/lib64/
 
-#eval LD_PRELOAD=/home/carol/git_research/nvbitfi_ml/nvbitfi/profiler/profiler.so ./test_cublas
-eval LD_PRELOAD=/home/carol/git_research/nvbitfi_ml/nvbitfi/injector/injector.so ./test_cublas
+eval LD_PRELOAD=../../pf_injector_icoc/pf_injector.so ./gemm
 
 
--- test-apps/gemm_cublas/gemm.cu
+++ test-apps/gemm_cublas/gemm.cu
@@ -10,25 +10,29 @@
 #include "cuda_utils.h"
 #include "device_vector.h"
 
-
 void call_gemm(size_t dim, const half *v1_dev, const half *v2_dev, half *output_dev, cublasHandle_t blas_handle,
-                     half &alpha, half &beta){
+               half &alpha, half &beta) {
     checkCublasErrors(cublasHgemm(blas_handle, CUBLAS_OP_N, CUBLAS_OP_N, dim, dim, dim,
                                   &alpha, v1_dev, dim, v2_dev, dim, &beta, output_dev, dim));
 }
 
 void call_gemm(size_t dim, const float *v1_dev, const float *v2_dev, float *output_dev, cublasHandle_t blas_handle,
-                      float &alpha, float &beta){
+               float &alpha, float &beta) {
     checkCublasErrors(cublasSgemm(blas_handle, CUBLAS_OP_N, CUBLAS_OP_N, dim, dim, dim,
                                   &alpha, v1_dev, dim, v2_dev, dim, &beta, output_dev, dim));
 }
 
+void call_gemm(size_t dim, const double *v1_dev, const double *v2_dev, double *output_dev, cublasHandle_t blas_handle,
+               double &alpha, double &beta) {
+    checkCublasErrors(cublasDgemm(blas_handle, CUBLAS_OP_N, CUBLAS_OP_N, dim, dim, dim,
+                                  &alpha, v1_dev, dim, v2_dev, dim, &beta, output_dev, dim));
+}
 
 template<typename real_t>
 void perform_gemm(size_t dim, real_t val_v1, real_t val_v2) {
     auto size = dim * dim;
     std::cout << "Size of the array:" << size << std::endl;
-    std::vector <real_t> v1(size, val_v1), v2(size, val_v2);
+    std::vector<real_t> v1(size, val_v1), v2(size, val_v2);
     DeviceVector<real_t> v1_dev = v1;
     DeviceVector<real_t> v2_dev = v2;
     DeviceVector<real_t> output_dev(size, 0);
@@ -60,14 +64,18 @@ void perform_gemm(size_t dim, real_t val_v1, real_t val_v2) {
 
 int main(int argc, char *argv[]) {
     setbuf(stdout, nullptr); // Disable stdout buffering
+    if (argc < 2) {
+        std::cerr << "You should enter " << argv[0] << " [half|float|double]\n";
+    }
+    auto precision = std::string(argv[1]);
     auto size = 256;
-    perform_gemm<float>(size, 1.0f, 1.0f);
-#if PRECISION == 16
-    perform_gemm<half>(size, 1.0f, 1.0f);
-#elif PRECISION == 32
-    perform_gemm<float>(size, 1.0f, 1.0f);
-#elif PRECISION == 64
-    perform_gemm<double>(size, 1.0f, 1.0f);
-#endif
+    if (precision == "half") {
+        perform_gemm<half>(size, 1.0f, 1.0f);
+    } else if (precision == "float") {
+        perform_gemm<float>(size, 1.0f, 1.0f);
+    } else if (precision == "double") {
+        perform_gemm<double>(size, 1.0f, 1.0f);
+    }
+
     return 0;
 }
