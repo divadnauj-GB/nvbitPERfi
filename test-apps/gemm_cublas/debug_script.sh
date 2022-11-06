35525eb60dea50b2b96e9b8c6a5dae6917cfd398
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2022-11-06 18:00:24 +0100

    Making GEMM a real workload

--- test-apps/gemm_cublas/debug_script.sh
+++ test-apps/gemm_cublas/debug_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # environment variables for NVBit
 #export NOBANNER=1
 # set TOOL_VERBOSE=1 to print debugging information during profling and injection runs
@@ -11,6 +14,10 @@ export CUDA_BASE_DIR=/usr/local/cuda
 export PATH=$PATH:$CUDA_BASE_DIR/bin
 export LD_LIBRARY_PATH=$CUDA_BASE_DIR/lib64/:$CUDA_BASE_DIR/extras/CUPTI/lib64/
 
-eval LD_PRELOAD=../../pf_injector_icoc/pf_injector.so ./gemm float
+LD_PRELOAD_LIB_PATH=../../pf_injector_icoc
+
+make -C $LD_PRELOAD_LIB_PATH
+
+time eval LD_PRELOAD=$LD_PRELOAD_LIB_PATH/pf_injector.so ./gemm float a.data b.data gold.data 1
 
 
--- test-apps/gemm_cublas/run.sh
+++ test-apps/gemm_cublas/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eval ${PRELOAD_FLAG} ${BIN_DIR}/test_cublas > stdout.txt 2> stderr.txt
+eval ${PRELOAD_FLAG} ${BIN_DIR}/gemm float ${BIN_DIR}/a.data ${BIN_DIR}/b.data ${BIN_DIR}/gold.data 0 > stdout.txt 2> stderr.txt
