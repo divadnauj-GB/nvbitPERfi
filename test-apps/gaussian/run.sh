fb81a2bec105f7407262973dfc2d11359311d740
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2022-11-15 12:34:10 +0100

    Fix gaussian app run.

--- test-apps/gaussian/run.sh
+++ test-apps/gaussian/run.sh
@@ -6,8 +6,8 @@ SIZE=$3
 
 DATA_DIR=${BIN_DIR}
 
-eval LD_LIBRARY_PATH="${LIB_LOG_HELPER}":"${CUDAPATH}"/lib64:"$LD_LIBRARY_PATH"  "${PRELOAD_FLAG}" \
- "${BIN_DIR}"/cudaGaussian --size "${SIZE}" --input "${DATA_DIR}"/input_"${SIZE}".data \
- --gold "${DATA_DIR}"/gold_"${SIZE}".data --iterations 1 --verbose > stdout.txt 2>stderr.txt
+eval LD_LIBRARY_PATH="${LIB_LOG_HELPER}":"${CUDAPATH}"/lib64:"$LD_LIBRARY_PATH" "${PRELOAD_FLAG}" \
+"${BIN_DIR}"/cudaGaussian --size "${SIZE}" --input "${DATA_DIR}"/input_"${SIZE}".data \
+--gold "${DATA_DIR}"/gold_"${SIZE}".data --iterations 1 --verbose >stdout.txt 2>stderr.txt
 
 sed -i '/time/c\' stdout.txt 
