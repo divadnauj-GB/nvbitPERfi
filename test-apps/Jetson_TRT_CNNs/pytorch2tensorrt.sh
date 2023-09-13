9fd851dccb46d22bcd4fe71d6db142a813ab0a26
divadnauj-GB <juan.guerrero@polito.it>
2023-09-13 12:01:43 +0200

    removed useless folders

--- test-apps/Jetson_TRT_CNNs/pytorch2tensorrt.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-APP_ARGS=$*
-
-python3 ${APP_ARGS} > stdout.txt 2> stderr.txt
-
--- test-apps/Pytorch_CNNs/run.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-APP_ARGS=$*
-
-#source ~/miniconda3/etc/profile.d/conda.sh
-#conda activate Pytorch_nvbitPERfi
-
-eval ${PRELOAD_FLAG} python ${BIN_DIR}/${APP_BIN} ${APP_ARGS} > stdout.txt 2> stderr.txt
